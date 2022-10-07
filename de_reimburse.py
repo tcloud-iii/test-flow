b2ca78afaf3a32f32b77bf0a4cdb40de769d6a72
tcloud-iii <115130391+tcloud-iii@users.noreply.github.com>
2022-10-07 11:50:56 +0800

    Update de_reimburse.py

--- de_reimburse.py
+++ de_reimburse.py
@@ -3,20 +3,26 @@
 import pandas as pd
 import os
 
-#directory_name = '/home/tcloud_iii_gcp/test-flow'
 directory_name = '/home/tcloud_iii_gcp/test-flow'
 
 
 for file_name in os.listdir(directory_name):
   if '每日可核銷訂單清單' in file_name:
-    reimfile = os.path.join(directory_name, file_name)
+      if '.xlsx' in file_name:
+          reimfile = os.path.join(directory_name, file_name)
 
 
 reimburse = pd.read_excel(reimfile)
 
-reim_col =['supplier_ban', 'sme_ban', 'order_num', 'order_status', 'reimburse_status', 'reimburse_num', 'contract_start_date', 'contract_end_date', 'solution_uuid', 'solution_name', 'solution_spec', 'solution_duration', 'sol_price', 'sol_point', 'sol_selfpay', 'return_point', 'return_pay',
-'last_reim_point', 'now_reim_point', 'rest_point', 'receipt_status', 'reimburse_submit_date', 'reim_change_date', 'order_change_date', 'sme_name', 'supplier_name', 'reimburseful_date', 'solution_satisfaction', 'reviews', 'invoice1', 'invoice_check1', 'invoice2', 'invoice_check2', 'point_source',
-'point_source_code', 'unreimbuse_point', 'invoice_date1', 'invoice_date2', 'invoice_price1', 'invoice_price2']
+reim_col =['supplier_ban', 'sme_ban', 'order_num', 'order_status', 'reimburse_status',
+ 'reimburse_num', 'contract_start_date', 'contract_end_date', 'solution_uuid', 'solution_name', 
+ 'solution_spec', 'solution_duration', 'sol_price', 'sol_point', 'sol_selfpay', 
+ 'return_point', 'return_pay','last_reim_point', 'now_reim_point', 'rest_point', 'receipt_status', 
+ 'reimburse_submit_date', 'reim_change_date', 'order_change_date', 'sme_name', 'supplier_name', 
+ 'reimburseful_date', 'solution_satisfaction', 'reviews', 'invoice1', 'invoice_check1', 'invoice2', 
+ 'invoice_check2', 'point_source', 'point_source_code', 'unreimbuse_point', 'invoice_date1', 
+ 'invoice_date2', 'invoice_price1', 'invoice_price2']
+
 reimburse.columns =reim_col
 
 
