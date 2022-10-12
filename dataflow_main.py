6d6f92b845a35c2fe6298a6f0939044fe325f728
tcloud-iii <115130391+tcloud-iii@users.noreply.github.com>
2022-10-12 19:16:47 +0800

    Update dataflow_main.py

--- dataflow_main.py
+++ dataflow_main.py
@@ -1,15 +1,15 @@
 import os
 #下載、解壓縮、檔案zip送到GCS
-os.system('python3 get_zipfiles.py')
+os.system('python3 /home/tcloud_iii_gcp/test-flow/get_zipfiles.py')
 
 #資料轉格式
 directory_name = '/home/tcloud_iii_gcp/test-flow'
 
 for file in os.listdir(directory_name):
     if 'de_' in file:
-      pyfile = 'python3 '+file
+      pyfile = 'python3 '+'directory_name'+'/'+file
       os.system(pyfile)
 print("Files proceed done!")
 
 #清除檔案
-os.system('python3 file_remove.py')
+os.system('python3 /home/tcloud_iii_gcp/test-flow/file_remove.py')
