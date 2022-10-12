f1d3096ba19e62c53200c5a2c02e81f18e791c24
tcloud-iii <115130391+tcloud-iii@users.noreply.github.com>
2022-10-12 13:27:11 +0800

    Update get_zipfiles.py

--- get_zipfiles.py
+++ get_zipfiles.py
@@ -7,7 +7,7 @@ import os
 
 directory_name = '/home/tcloud_iii_gcp/test-flow/'
 #正式站url
-x = requests.get('https://test-manage.tcloud.gov.tw/gdp-backend-api/api/hpc/download',
+x = requests.get('https://manage.tcloud.gov.tw/gdp-backend-api/api/hpc/download',
                  params={"downloadPath": '/apiRecordLog/newst/最新產檔結果.zip',
                          'key': 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIvYXBpUmVjb3JkTG9nL25ld3N0L-acgOaWsOeUouaqlOe1kOaenC56aXAiLCJleHAiOjE2OTcwNzAwMDIsImlhdCI6MTY2NTUzNDAwMn0.iGaOd825-CVgw034ME_E7XlopvVpTB2tQ7Cgi1pXIQPTbWHW2zLhWOLaRWHl2JhAkdndH4HxUiuy8JYvDwMcCg',
                          'iv':'1665534002425'
@@ -20,3 +20,4 @@ with zipfile.ZipFile(directory_name+"today.zip","r") as zip_ref:
 
 with zipfile.ZipFile(directory_name+"最新產檔結果.zip","r") as zip_ref:
     zip_ref.extractall(directory_name)
+
