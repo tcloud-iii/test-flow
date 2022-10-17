283cb541195180dad68403d276e270db23037a09
tcloud-iii <115130391+tcloud-iii@users.noreply.github.com>
2022-10-17 14:47:18 +0800

    Update de_reimburse.py

--- de_reimburse.py
+++ de_reimburse.py
@@ -24,7 +24,16 @@ reim_col =['supplier_ban', 'sme_ban', 'order_num', 'order_status', 'reimburse_st
  'invoice_date2', 'invoice_price1', 'invoice_price2']
 
 reimburse.columns =reim_col
+#改欄位型別
+#改欄位型別
+datetp =['contract_start_date','contract_end_date','reimburse_submit_date','reim_change_date',
+'order_change_date']
+for i in datetp:
+  reimburse[i] = pd.to_datetime(reimburse[i])
 
+strtp =['supplier_ban','sme_ban', 'order_num']
+for j in strtp:
+    reimburse[j] = reimburse[j].astype('str') 
 
 reimburse.to_csv(directory_name+'/'+'reimburse.csv')
 
@@ -57,3 +66,24 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
 upload_blob('tcloud_bq_files',directory_name+'/reimburse.csv','reimburse.csv')
 
 print('Reimburse file has done!')
+
+#to bigquery
+from google.cloud import bigquery
+credentials_path = "/home/tcloud_iii_gcp/front_to_bq.json"
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+client = bigquery.Client()
+#table_id = 'tcloud-data-analysis.tcloud_analytic_db.order_basic'
+ 
+#job = client.load_table_from_dataframe(point_mon, table_id)
+#job.result()  #等待寫入完成
+
+#reimburse 
+dataset_ref = client.dataset('tcloud_analytic_db')
+table_ref = dataset_ref.table('reimburse')
+job_config = bigquery.job.LoadJobConfig()
+job_config.write_disposition = bigquery.WriteDisposition.WRITE_TRUNCATE
+client.load_table_from_dataframe(reimburse, table_ref, job_config=job_config)
+
+
+
+print('Reimburse  file has sent to Bigquery!')
