2197e7cf98cd2eb76fd01c999b5fe1b9dfcc8b6e
tcloud-iii <115130391+tcloud-iii@users.noreply.github.com>
2022-10-18 15:39:57 +0800

    Update get_zipfiles.py

--- get_zipfiles.py
+++ get_zipfiles.py
@@ -21,3 +21,34 @@ with zipfile.ZipFile(directory_name+"today.zip","r") as zip_ref:
 with zipfile.ZipFile(directory_name+"最新產檔結果.zip","r") as zip_ref:
     zip_ref.extractall(directory_name)
 
+# 這邊要吃api金鑰
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/tcloud_iii_gcp/front_to_bq.json"
+storage_client = storage.Client()
+
+#主要的fuction
+def upload_blob(bucket_name, source_file_name, destination_blob_name):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        f"File {source_file_name} uploaded to {destination_blob_name}."
+    )
+
+from datetime import date
+today = date.today()
+filenm ='最新產檔結果_'+str(today)+'.zip'
+print(filenm)
+
+#測試上傳單一檔案，第一個參數是傳送位置(不動)，第二個是檔案路徑，第三個是檔案名稱
+upload_blob('tradevan-to-iii',directory_name+'最新產檔結果.zip',filenm)
